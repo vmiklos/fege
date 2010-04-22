bc9e62c1ca9fa0d224bc003c8ae51bfb083d1d99
Miklos Vajna <vmiklos@frugalware.org>
2010-04-23 00:47:40 +0200

    psconv: kghostview elkerulese

--- psconv.sh
+++ psconv.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# fuzteljes.ps generalasa: nyomtatas kden keresztul pdfbe majd kpdfbol
-# nyomtatas psbe
-# baz.pdf generalasa: nyomtatas kghostview-ban pdfbe
-# (tehat ps2pdf es pdf2ps nem jo)
+# fuzteljes.ps generalasa: nyomtatas pdfbe (cutepdf) majd okularbol nyomtatas
+# psbe
+# (tehat pdf2ps nem jo)
 
 pstops "1:0R@1.42(0,1h),0R@1.42(-1w,1h)" fuzteljes.ps foo.ps
 psbook -s4 foo.ps bar.ps
 psnup -2 bar.ps baz.ps
+ps2pdf -sPAPERSIZE=a4 baz.ps
